3baf9ad70ed97bdb427f7142e7404549fa51b995
andyCarol <119090440+andyCarol@users.noreply.github.com>
2022-11-25 22:46:23 -0600

    Add files via upload

--- GalletasAngulo.java
+++ GalletasAngulo.java
@@ -5,7 +5,45 @@ import java.util.Scanner;
 public class GalletasAngulo{
    public static void main(String args[]){
       Scanner leer = new Scanner(System.in);
-      System.out.println("GALLETAS ANGULO");
-      System.out.print("si");
+      int resp;
+      
+           
+      //inicio do-while
+      do {
+          //Menú
+      System.out.println("INVENTARIO GALLETAS ANGULO");
+      System.out.print("¿Qué es lo que desea hacer? [1-6]");
+      System.out.println("[1] Registro de galletas");
+      System.out.println("[2] Consultar tipo de galleta");
+      System.out.println("[3] Consultar todas las galletas");
+      System.out.println("[4] Modificar datos de galletas");
+      System.out.println("[5] Eliminar datos de galletas");
+      System.out.println("[6] Finalizar");
+      resp = leer.nextInt();
+
+         
+         switch (resp){
+            case1:
+            
+               break;
+            case2:
+            
+               break;
+            case3:
+            
+               break;
+            case4:
+            
+               break;
+            case5:
+            
+               break;
+            case6:
+               System.out.println("Programadores:De La Cruz Cervantes Ramses & Roman García Andrea Carolina");
+               System.out.println("Fecha: 03/Diciembre/2022");
+               break;
+            default: System.out.println("!ERROR numero no valido");
+         }
+      } while (resp!=6);
    }
 }
