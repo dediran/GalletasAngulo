8007722758534455379db033b9a7ab5fb64c5822
Ramses <ramsesd61@gmail.com>
2022-11-25 20:34:19 -0600

    Se enpieza el proyecto

---
+++ GalletasAngulo.java
@@ -0,0 +1,10 @@
+/*
+   VERSION 0
+*/
+import java.util.Scanner;
+public class GalletasAngulo{
+   public static void main(String args[]){
+      Scanner leer = new Scanner(System.in);
+      System.out.println("GALLETAS ANGULO");
+   }
+}
