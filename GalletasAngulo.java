5133f9b0c46671426c3205670afca7f84e98dc04
Ramses <117543662+dediran@users.noreply.github.com>
2022-11-26 23:29:14 -0600

    Add files via upload
    terminacion de  las altas con sus restricciones terminadas

--- GalletasAngulo.java
+++ GalletasAngulo.java
@@ -6,14 +6,14 @@ import java.util.Scanner;
 public class GalletasAngulo{
    public static void main(String args[]){
       Scanner leer = new Scanner(System.in);
-      int claves[] = new int[10];
-      String nombres[] = new String[10];
-      float precios[] = new float[10];
-      char tamallos[] = new char[10];
-      boolean stock[] = new boolean[10];
-      int resp,cont = 0;
+      int claves[] = new int[4];
+      String nombres[] = new String[4];
+      float precios[] = new float[4];
+      char tamallos[] = new char[4];
+      boolean stock[] = new boolean[4];
+      int resp,cont = 1;
       char puntoBool;
-      boolean indicador = true;
+      boolean indicador = true,espacio = true;
       
           
       do {
@@ -29,46 +29,64 @@ public class GalletasAngulo{
       
          switch (resp){
             case 1:
-               System.out.printf("\nclave[%d]: ",cont);
-               claves[cont] = leer.nextInt();
-               for(int i = -1; i<cont; i++){
-                  if(cont == 0 && i == -1){
-                     indicador = true;
-                     break;
+                  if (cont-1 == claves.length){
+                     espacio = false;   
+                  }else {
+                     espacio = true;
                   }
-                  if(claves[cont] == claves[i+1]){
-                     indicador = false;              
-                     break;
+               if(espacio){
+                  do{
+                     System.out.printf("\nclave[%d]: ",cont-1);
+                     claves[cont-1] = leer.nextInt();
+                     if(claves[cont-1] >= 1 && claves[cont-1] <= 111){
+                        indicador = false;
+                     }
+                  }while(indicador);
+                  for(int i = 0; i<cont; i++){
+                     if(cont == 1 && i == 0){
+                        indicador = true;
+                        break;
+                     }
+                     if(claves[cont-1] == claves[i]){
+                        if(cont-1 == i){
+                           break;
+                        }
+                        indicador = false;
+                        claves[cont-1] = 0;              
+                        break;
+                     }else{
+                        indicador = true;
+                     }                
+                  }
+                  if(indicador){
+                     leer.nextLine();
+                     System.out.printf("nombre del paquete[%d]",cont-1);
+                     nombres[cont-1] = leer.nextLine();
+                     System.out.printf("precio del paquete[%d]",cont-1);
+                     precios[cont-1] = leer.nextFloat();
+                     System.out.printf("tamaño del paquete[%d]",cont-1);
+                     tamallos[cont-1] = leer.next().charAt(0);
+                     System.out.printf("hay disponibles en el almacen? [s = SI] , [n = NO]");
+                     puntoBool = leer.next().charAt(0);
+                     if(puntoBool == 's'|| puntoBool == 'S'){
+                        stock[cont-1] = true;
+                     }else if(puntoBool == 'n' || puntoBool == 'N'){
+                        stock[cont-1] = false;
+                     }
                   }else{
-                     indicador = true;
-                  }                
-               }
-               if(indicador){
-                  leer.nextLine();
-                  System.out.printf("nombre del paquete[%d]",cont);
-                  nombres[cont] = leer.nextLine();
-                  System.out.printf("precio del paquete[%d]",cont);
-                  precios[cont] = leer.nextFloat();
-                  System.out.printf("tamaño del paquete[%d]",cont);
-                  tamallos[cont] = leer.next().charAt(0);
-                  System.out.printf("hay disponibles en el almacen? [s = SI] , [n = NO]");
-                  puntoBool = leer.next().charAt(0);
-                  if(puntoBool == 's'|| puntoBool == 'S'){
-                     stock[cont] = true;
-                  }else if(puntoBool == 'n' || puntoBool == 'N'){
-                     stock[cont] = false;
+                     System.out.println("ESTA CLAVE ESTA REPETIDA\n");
+                     cont--;
                   }
+                  cont++;
                }else{
-                  System.out.println("ESTA CLAVE ESTA REPETIDA\n");
-                  cont--;
-               }
-               cont++;
+                  System.out.println("\nEL ESPACIO ESTA LLENO\n");
+               }   
                break;
             case 2:
             
                break;
             case 3:
-               for(int x = 0; x < 10; x++){
+               for(int x = 0; x < 4; x++){
                   System.out.printf("clave[%d] = %d | nombre[%d] = %s  | precio[%d] = %.2f  | tamaño[%d] = %S   |  stock[%d] = %s\n", x, claves[x], x, nombres[x], x, precios[x], x, tamallos[x], x, stock[x]);
                }
                break;
